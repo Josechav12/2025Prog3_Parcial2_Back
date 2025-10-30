953b743c828118730442bbbf4d2124cf2b24cc53
Josechav12 <josechavarri12@gmail.com>
2025-10-30 17:29:11 -0300

    ordenando

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,47 @@
-/*====================
-    Importaciones
-====================*/
+
 import express from "express";
 const app = express();
 
-import environments from "./src/api/config/environments.js";
-
+import environments from "./src/api/config/environments.js"; // Importamos las variables de entorno para definir el puerto
 const PORT = environments.port;
 
+import connection from "./src/api/database/db.js"; // Importamos la conexion a la BBDD para poder enviarle sentencias SQL
+import cors from "cors"; // Importamos el modulo CORS para gestionar las politicas de seguridad de nuestro backend
 
-/*==================
-    Endpoints
-==================*/
+
+
+app.use(cors()); //Middleware CORS basico que permite todas las solicitudes
+
+
+
 app.get("/", (req, res) => {
-    res.send("Holis mundo desde Express.js");
+    res.send("apgina principal del backend");
+});
+
+// Traer todos los productos
+app.get("/products", async (req, res) => {
+
+    try {
+        const sql = "SELECT * FROM productos";
+    
+        // Con rows extraemos exclusivamente los datos que solicitamos en la consulta
+        const [rows] = await connection.query(sql);
+
+        console.log(rows);
+        
+        res.status(200).json({
+            payload: rows
+        });
+        
+    
+    } catch (error) {
+        console.error("Error obteniendo productos", error.message);
+
+        res.status(500).json({
+            message: "Error interno al obtener productos"
+        });
+    }
+
 });
 
 
